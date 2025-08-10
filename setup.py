1a5f56d9844e723385b3f6eb37938f45eb873958
Andrew <andrew@greatstrength.me>
2025-08-09 17:01:27 -0700

    9782356344 - Tiferet Monday - Update Package Structure/Setup (#3)

---
+++ setup.py
@@ -0,0 +1,33 @@
+try:
+    from setuptools import setup
+except:
+    from distutils.core import setup
+
+config = {
+    'description': 'Tiferet Monday, a Tiferet app for managing Monday.com boards and items.',
+    'author': 'Andrew Shatz, Great Strength Systems',
+    'url': r'https://github.com/greatstrength/tiferet-monday',
+    'download_url': r'https://github.com/greatstrength/tiferet-monday',
+    'author_email': 'andrew@greatstrength.me',
+    'version': '0.1.0a0',
+    'license': 'BSD 3',
+    'install_requires': [
+        'tiferet>=1.1.0',
+        'moncli>=2.0.0',
+    ],
+    'packages': [
+        'tiferet_monday',
+        'tiferet_monday.commands',
+        'tiferet_monday.configs',
+        'tiferet_monday.contracts',
+        'tiferet_monday.proxies',
+        'tiferet_monday.proxies.monday',
+    ],    
+    'scripts': [],
+    'name': 'tiferet-monday',
+    'extras_require': {
+        'test': ['pytest>=8.3.3', 'pytest_env>=1.1.5'],
+    }
+}
+
+setup(**config)
