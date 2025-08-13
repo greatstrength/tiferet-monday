df14f723a120d46799e9da839fa05ed61e62f056
Ashatz <andrew@greatstrength.me>
2025-08-13 09:51:53 -0700

    Proxies - Add Document Monday Proxy

---
+++ tiferet_monday/proxies/monday/doc.py
@@ -0,0 +1,62 @@
+# *** imports
+
+# ** app
+from ...contracts.doc import *
+from ...data.doc import *
+from . import MondayApiProxy
+
+# *** proxies
+
+# ** proxy: document_monday_proxy
+class DocumentMondayProxy(MondayApiProxy, DocumentRepository):
+    """
+    Proxy for managing document-related operations using the Monday.com API.
+    """
+
+    # * init
+    def __init__(self, monday_api_key: str):
+        """
+        Initializes the DocumentMondayProxy with the Monday.com API key.
+
+        :param monday_api_key: API key for accessing the Monday.com API.
+        :type monday_api_key: str
+        """
+        
+        # Initialize the parent class with the API key.
+        super().__init__(monday_api_key)
+
+    # * method: create_doc_in_column
+    def create_doc_in_column(self, item_id: str | int, column_id: str) -> DocumentContract:
+        """
+        Creates a document in the specified column of an item using the Monday.com API.
+
+        :param item_id: ID of the item where the document will be created.
+        :type item_id: str | int
+        :param column_id: ID of the column where the document will be created.
+        :type column_id: str
+        :return: The created document.
+        :rtype: DocumentContract
+        """
+        
+        # Execute the mutation to create a document in the specified column.
+        data = self.execute_query(
+            mutation="""
+                mutation ($itemId: ID!, $columnId: String!) {
+                    create_doc(location: {board: {item_id: $itemId, column_id: $columnId}}) {
+                        id
+                        name
+                        object_id
+                    }
+                }
+            """,
+            variables={
+                'item_id': int(item_id),
+                'column_id': column_id
+            }
+        )
+
+        # Return the created document as a DocumentContract instance.
+        return DataObject.from_data(
+            DocumentData,
+            **data
+        ).map()
