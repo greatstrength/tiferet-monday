7cdee41d983b074c3b6e1331340f870ecdf727b7
Ashatz <andrew@greatstrength.me>
2025-08-13 09:33:22 -0700

    Contracts - Add Document and Document Repository

---
+++ tiferet_monday/contracts/doc.py
@@ -0,0 +1,42 @@
+# *** imports
+
+# ** infra
+from tiferet.contracts import *
+
+# *** contracts
+
+# ** contract: document
+class DocumentContract(ModelContract):
+    """
+    Represents a document in the Tiferet Monday application.
+    """
+
+    # * attribute: id
+    id: str
+
+    # * attribute: name
+    name: str
+
+    # * attribute: object_id
+    object_id: str
+
+# ** contract: document_repo
+class DocumentRepository(Repository):
+    """
+    Repository for managing document-related operations.
+    """
+
+    # * method: create_doc_in_column
+    @abstractmethod
+    def create_doc_in_column(self, board_id: str | int, column_id: str) -> DocumentContract:
+        """
+        Creates a document in the specified column of a board.
+
+        :param board_id: ID of the board where the document will be created.
+        :type board_id: str | int
+        :param column_id: ID of the column where the document will be created.
+        :type column_id: str
+        :return: The created document.
+        :rtype: DocumentContract
+        """
+        raise NotImplementedError('create_doc_in_column method must be implemented in the board_repo.')
