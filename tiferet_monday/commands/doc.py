d83eb031143fd70a64997e3308a028358141df1b
Ashatz <andrew@greatstrength.me>
2025-08-21 08:57:03 -0700

    Commads - Add Command

--- tiferet_monday/commands/doc.py
+++ tiferet_monday/commands/doc.py
@@ -84,3 +84,40 @@ class UpdateDocName(Command):
         
         # Call the repository method to update the document name.
         self.document_repo.update_doc_name(doc_id=doc_id, name=name)
+
+# ** command: read_doc_blocks
+class ReadDocBlocks(Command):
+    """
+    Command to read the blocks of a specified document.
+    """
+
+    # * attribute: document_repo
+    document_repo: DocumentRepository
+
+    # * init
+    def __init__(self, document_repo: DocumentRepository):
+        """
+        Initializes the ReadDocBlocks command with the document repository.
+
+        :param document_repo: The repository for managing document operations.
+        :type document_repo: DocumentRepository
+        """
+        
+        # Assign the document repository to the command instance.
+        self.document_repo = document_repo
+
+    # * method: execute
+    def execute(self, doc_id: str | int, **kwargs) -> List[DocumentBlock]:
+        """
+        Executes the command to read blocks from a specified document.
+
+        :param doc_id: ID of the document to read blocks from.
+        :type doc_id: str | int
+        :param kwargs: Additional keyword arguments.
+        :type kwargs: dict
+        :return: List of blocks in the document.
+        :rtype: List[DocumentBlockContract]
+        """
+        
+        # Call the repository method to read the document blocks.
+        return self.document_repo.read_doc_blocks(doc_id=doc_id)
