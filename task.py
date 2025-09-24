b8fddacc9967fdcff33c7f08dd9cec93ed20e549
Ashatz <andrew@greatstrength.me>
2025-09-24 09:58:46 -0700

    Scripts - Tifert Composer Tui and Other Updates

--- task.py
+++ task.py
@@ -3,6 +3,9 @@ from tiferet_monday.models import (
     StatusValue
 )
 from monday_app import app
+from typing import List, Callable, Any, Tuple
+from tiferet import ModelObject
+from tiferet_monday.models import Item
 
 def task_id_input() -> str:
     '''
@@ -16,15 +19,16 @@ def task_id_input() -> str:
     if task_id == '':
         print('Exiting...')
         exit(0)
+    print('')
     return task_id
 
-def retrieve_task_details(task_id: str) -> ItemDetail:
+def retrieve_task_details(task_id: str, is_subtask: bool = False) -> ItemDetail:
     '''
     Retrieve the details of a task by its ID.
     :param task_id: The ID of the task to retrieve.
     :return: An ItemDetail object containing the task details.
     '''
-    print('Retrieving task details...')
+    print('Retrieving subtask details...') if is_subtask else print('Retrieving task details...\n')
     task = ItemDetail.new(
         **app.run('item.query_detail_by_id', data=dict(
             item_id=task_id
@@ -32,35 +36,62 @@ def retrieve_task_details(task_id: str) -> ItemDetail:
     )
     return task
 
-def list_subtasks(task: ItemDetail) -> list[ItemDetail]:
+def list_subtasks(task_id) -> list[ItemDetail]:
     '''
     List all subtasks of a given task.
-    :param task: The ItemDetail object representing the task.
+
+    :param task_id: The ID of the parent task.
+    :type task_id: str
     :return: A list of ItemDetail objects representing the subtasks.
+    :rtype: list[ItemDetail]
     '''
-    print('Listing subtasks...')
+
+    print('\nRetrieving subtasks...\n')
     subtasks_data = app.run('item.query_subitems', data=dict(
-        parent_item_id=task.id
+        parent_item_id=task_id
     ))
     return [
-        ItemDetail.new(**s) for s in subtasks_data
+        ModelObject.new(
+            Item,
+            **s) for s in subtasks_data
     ]
 
-def get_task_status(task: ItemDetail) -> StatusValue:
-    '''
-    Get the status of a task.
-    :param task: The ItemDetail object representing the task.
-    :return: The status text of the task.
-    '''
+def retrieve_subtask_details(subtask_id: str, column_ids: list[str]) -> Tuple[Any]:
+    return app.run('item.query_column_values', data=dict(
+    item_id=subtask_id,
+    column_ids=column_ids
+))
 
-    # 
-    print('Retrieving task status...')
-    task_status = task.get_column_value('Status')
-    task_status = app.run('item.query_column_values', data=dict(
-        item_id=task.id,
-        column_ids=[task_status.id]
-    ))[0]
-    return task_status
+def get_status_text(task: ItemDetail) -> str:
+    '''
+    Get the status text of a task.
+
+    :param task: The ItemDetail object representing the task.
+    :type task: ItemDetail
+    :return: The status text of the task.
+    :rtype: str
+    '''
+    status_column = task.get_column_value('Status')
+    if not status_column:
+        raise ValueError('Status column not found in the task.')
+    return status_column.text
+
+def begin_subtask(subtask: ItemDetail, owner_column_id: str, owner_id: str, status_column_id: str):
+    
+    print('Assigning subtask to yourself...')
+    app.run('item.update_simple_column_value', data=dict(
+        item_id=subtask.id,
+        column_id=owner_column_id,
+        value=owner_id)
+    )
+
+
+    print('Updating subtask status to Working on it...') # Delete This
+    app.run('item.update_simple_column_value', data=dict(
+        item_id=subtask.id,
+        column_id=status_column_id,
+        value='Working on it')
+    )
 
 def get_branch_name(task: ItemDetail) -> str:
     '''
--- tiferet_composer_tui.py
+++ tiferet_composer_tui.py
@@ -1,27 +1,34 @@
 from monday_app import app
-import os
-from typing import Callable
+import os, json
+from typing import List, Callable
 
-from tiferet_monday.models.item import ItemDetail, StatusValue
+from tiferet_monday.models.item import ItemDetail, Item
 
 from git import (
     pull_latest_from_base_branch,
     create_and_push_branch,
     checkout_branch,
+    stage_file,
+    commit_changes,
     delete_local_branch
 )
 from task import (
     task_id_input,
+    get_status_text,
     retrieve_task_details,
-    get_task_status,
     get_branch_name,
+    list_subtasks,
+    begin_subtask,
     update_subtask_status_to_done
 )
 
 ROOT_PATH = os.getcwd()
-
+PROJECT_ROOT = os.getenv('TIFERET_APP_PROJECT_ROOT')
 GITHUB_BASE_BRANCH = 'main'
+TASK_OWNER_ID = '39073543'
 
+
+# * function: create_dev_branch
 def create_dev_branch(task: ItemDetail, back: Callable = lambda d: d):
     '''
     Create and push a new development branch for a subtask if the task is ready to start.
@@ -47,14 +54,15 @@ def create_dev_branch(task: ItemDetail, back: Callable = lambda d: d):
     print('Task is in progress. Please proceed to committing subtask content...')
 
     back()
-    
-def remove_dev_branch(task: ItemDetail):
+
+# * function: remove_dev_branch    
+def remove_dev_branch(task: ItemDetail, back: Callable = lambda d: d):
     '''
     Delete a local development branch and switch back to the main branch if the task is done.
     '''
 
     # Update the status to done.
-    task_status = get_task_status(task)
+    task_status = task.get_column_value('Status')
     if task_status.text != 'Done':
         update_subtask_status_to_done(task, task_status)
 
@@ -62,8 +70,8 @@ def remove_dev_branch(task: ItemDetail):
     git_branch = get_branch_name(task)
 
     print(f'Deleting local branch {git_branch} and switching back to {GITHUB_BASE_BRANCH}...')
-    project_root = os.getenv('TIFERET_APP_PROJECT_ROOT')
-    os.chdir(project_root)
+    PROJECT_ROOT = os.getenv('TIFERET_APP_PROJECT_ROOT')
+    os.chdir(PROJECT_ROOT)
 
     # Pull the latest from the base branch.
     pull_latest_from_base_branch()
@@ -76,48 +84,229 @@ def remove_dev_branch(task: ItemDetail):
 
     print('Local branch deleted and switched back to main branch.')
 
-def select_subtasks(task: ItemDetail, back: Callable = lambda d: d):
+    back(task)
+
+# * function: select_subtask_from_list
+def select_subtask_from_list(subtasks: List[Item], back: Callable = lambda d: d):
     '''
-    List all subtasks of a given task.
+    Display a list of subtasks and allow the user to select one.
     '''
-    subtasks = app.run('item.query_subitems', data=dict(
-        parent_item_id=task.id
-    ))
     
     print('Select a subtask to work on:')
     for index, subtask in enumerate(subtasks, start=1):
-        print(f"{index}. {subtask['name']} (ID: {subtask['id']})")
-    print('Enter the number of the subtask (or press Enter to exit): ')
-    
-    return subtasks
+        print(f"{index}. {subtask.name} (ID: {subtask.id})")
 
+    # Get user input for subtask selection.
+    print('Enter the number of the subtask (or press Enter to exit): ')
+    option = input('> ')
+    if option == '':
+        print('Exiting...')
+        back()
+    option = int(option)
+    
+    # Retieve the selected subtask.
+    subtask = subtasks[option - 1]
+
+    # Retrieve the latest details of the selected subtask.
+    subtask_details = retrieve_task_details(subtask.id, is_subtask=True)
+
+    # Ensure the selected subtask is not already committed.
+    if subtask_details.get_column_value('Status').text == 'Committed':
+        print('Selected subtask has already been committed. Please select another subtask.')
+        return select_subtask_from_list(subtasks, back=back)
+    
+    print(f'You selected subtask: {subtask.name} (ID: {subtask.id})')
+
+    return subtask_details
+
+# * function: retrieve_subtask_code
+def retrieve_subtask_code(subtask: ItemDetail):
+    '''
+    Retrieve the code document associated with the subtask.
+    '''
+
+    print('Retrieving code document from item...')
+    code_new_after = subtask.get_column_value('Code (New/After)')
+    code_new_after = app.run('item.query_column_values', data=dict(
+        item_id=subtask.id,
+        column_ids=[code_new_after.id]
+    ))[0]
+
+    # Query the doc content using the object ID from the code column value
+    print('Read code from document...')
+    object_ids = [code_new_after.get_object_id()]
+    doc = app.run('doc.query_by_object_ids', data=dict(
+        object_ids=object_ids
+    ))[0]
+
+    # Extract the code from the doc content.
+    print('Extracting code from doc...')
+    blocks = doc.get('blocks', [])
+    content = blocks[0]['content'] if blocks else None
+    code_blocks = json.loads(content) if content else None
+    code = []
+    for block in code_blocks['deltaFormat']:
+        code.append(block['insert'])
+    return ''.join(code).replace('\n\n\n', '\n\n').replace('\xa0', ' ')
+
+
+# * function: write_code_to_file
+def write_code_to_file(subtask: ItemDetail, branch: str, code):
+    '''
+    Write the retrieved code to the appropriate file.
+    '''
+
+    print('Retrieving linked code module...')
+
+    # Retrieve the module column from the subtask.
+    module = subtask.get_column_value('Module')
+
+    # Retrieve the column value to get the linked item ID.
+    module = app.run(
+        'item.query_column_values',
+        data=dict(
+            item_id=subtask.id,
+            column_ids=[module.id]
+        )
+    )[0]
+
+    # Retrieve the linked item to get the file path.
+    linked_item_id = module.linked_item_ids[0]
+    linked_item: ItemDetail = app.run(
+        'item.query_detail_by_id', 
+        data=dict(
+            item_id=linked_item_id
+        )
+    )
+
+    # Change to the project root and write the code to the file.
+    os.chdir(PROJECT_ROOT)
+    checkout_branch(branch)
+    file_path = os.path.join(PROJECT_ROOT, linked_item.name)
+    print(f'Writing code to file: {file_path}...')
+    with open(file_path, 'w') as f:
+        f.write(code)
+    stage_file(file_path)
+    
+
+# * function: commit_changes_and_close_subtask
+def commit_changes_and_close_subtask(subtask: ItemDetail):
+    '''
+    Commit the changes to git and update the subtask status to Committed.
+    '''
+
+    commit = input("Wrote code to file. Press y to commit, any other key to exit: ")
+    if commit.lower() == 'y':
+        
+        # Commit and push the changes and return to the original path.
+        commit_changes(subtask.name)
+        os.chdir(ROOT_PATH)
+
+        subtask_status, actual_sp = subtask.get_column_values(['Status', 'Actual SP'])
+
+        # Update the status to Committed.
+        print('Updating subtask status to Committed...')
+        app.run('item.update_simple_column_value', data=dict(
+            item_id=subtask.id,
+            column_id=subtask_status.id,
+            value='Committed')
+        )
+
+        # Set the Actual SP column to 1.
+        print('Setting Actual SP...')
+        app.run('item.update_simple_column_value', data=dict(
+            item_id=subtask.id,
+            column_id=actual_sp.id,
+            value='1')
+        )
+
+# * function: work_dev_branch
+def work_dev_branch(task: ItemDetail, back: Callable = lambda d: d):
+    '''
+    Checkout and work an existing development branch for a subtask if the task is in progress.
+    '''
+
+    # List all subtasks of the task.
+    subtasks = list_subtasks(task.id)
+    if not subtasks:
+        print('No subtasks found for this task. Please create a subtask first.')
+        back()
+
+    while True:
+
+        # Let the user select a subtask to work on.
+        subtask = select_subtask_from_list(subtasks, back=lambda: sprint_task_management_menu(task, back=back))
+
+        # Retrieve the latest details of the selected subtask.
+        subtask = retrieve_task_details(subtask.id)
+        
+        # Retrieve the owner and status columns of the selected subtask.
+        owner, status = subtask.get_column_values(['Owner', 'Status'])
+
+        # Begin working on the selected subtask.
+        begin_subtask(
+            subtask, 
+            owner_column_id=owner.id, 
+            owner_id=TASK_OWNER_ID, 
+            status_column_id=status.id
+        )
+
+        # Retrieve the code associated with the subtask.
+        code = retrieve_subtask_code(subtask)
+
+        # Retrieve the git branch and write the code to the appropriate file.
+        git_branch = get_branch_name(task)
+        write_code_to_file(subtask, git_branch, code)
+
+        # Commit the changes and close the subtask.
+        commit_changes_and_close_subtask(subtask)
+
+        print('\nSubtask work completed.')
+        print('Do you want to work on another subtask? (y/n): ')
+        option = input('> ')
+        print('')
+        if option.lower() != 'y':
+            break
+
+# * function: sprint_task_management_menu
 def sprint_task_management_menu(task: ItemDetail, back: Callable = lambda d: d):
     '''
     Display a menu for managing sprint tasks.
     '''
 
     def create_menu_option():
-        if task_status.text == 'Ready to start':
+        if task_status_text == 'Ready to start':
             return 'Start working on this task (create and push a new dev branch)'
-        elif task_status.text == 'In Progress':
+        elif task_status_text == 'In Progress':
             return 'Continue working on this task (checkout existing dev branch)'
-        elif task_status.text == 'Pending Deployment':
+        elif task_status_text == 'Pending Deploy':
             return 'Finish working on this task (delete local dev branch and switch back to main)'
         else:
             return None
         
     def execute_task_option():
-        if task_status.text == 'Ready to start':
-            create_dev_branch(task, back=sprint_task_management_menu)
-        elif task_status.text == 'Pending Deployment':
+        if task_status_text == 'Ready to start':
+            create_dev_branch(task, back=lambda: sprint_task_management_menu(task.id))
+        elif task_status_text == 'In Progress':
+            work_dev_branch(task, back=main)
+        elif task_status_text == 'Pending Deploy':
             remove_dev_branch(task, back=sprint_task_management_menu)
-        
-    task_status = get_task_status(task)
-    print(f'Current task: {task.name} (Status: {task_status.text})')
+
     
-    print(f'1. {create_menu_option()}')
-    print('2. Back to main menu')
-    option = input('Enter 1 or 2 (or press Enter to exit): ')
+    task_status_text = get_status_text(task)
+
+    if task_status_text not in ['Ready to start', 'In Progress', 'Awaiting Review', 'Pending Deploy']:
+        print(f'Task status is "{task_status_text}". No actions available. Exiting...')
+        exit(0)
+
+    
+    
+    print('Select an option:')
+    
+    print(f'\t1. {create_menu_option()}')
+    print('\t2. Back to main menu')
+    print('Enter 1 or 2 (or press Enter to exit): ')
+    option = input('> ')
     if option == '':
         print('Exiting...')
         exit(0)
@@ -129,12 +318,13 @@ def sprint_task_management_menu(task: ItemDetail, back: Callable = lambda d: d):
         print('Invalid option. Exiting...')
         exit(0)
 
-
+# * function: main
 def main():
 
+    # Get the task ID from user input and retrieve the task details.
     task_id = task_id_input()
-
     task = retrieve_task_details(task_id)
+    print(f'Current task: {task.name} (Status: {get_status_text(task)})')
 
     sprint_task_management_menu(task, back=main)
     
