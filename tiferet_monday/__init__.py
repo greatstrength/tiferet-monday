4978a7caa4d75eafd9ee309d7bb0b5fb02ebb8c6
Ashatz <andrew@greatstrength.me>
2025-10-24 13:54:55 -0700

    App - Update Package Module Exports

--- tiferet_monday/__init__.py
+++ tiferet_monday/__init__.py
@@ -2,5 +2,13 @@
 
 # *** exports
 
+# ** app
+# Export the main application context and related modules.
+# Use a try-except block to avoid import errors on build systems.
+try:
+    from .proxies import MondayApiRequestsProxy as MondayApiProxy
+except:
+    pass
+
 # *** version
 __version__ = '0.1.0'
